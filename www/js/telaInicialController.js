20b1c8227fd17949a31d65ce2a5ed6f3f29ee5e1
Felipe <felipeems87@gmail.com>
2016-12-01 19:25:51 -0200

    Commitando ajustes para cancelar evento de soft and hard back buttons.

--- www/js/telaInicialController.js
+++ www/js/telaInicialController.js
@@ -1,9 +1,21 @@
 angular.module('starter.controllers')
 
-.controller('TelaInicialCtrl', ['$scope', '$stateParams', '$state', '$http', '$ionicPopup', '$ionicLoading',
-function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading) {
+.controller('TelaInicialCtrl', ['$scope', '$stateParams', '$state', '$http', '$ionicPopup', '$ionicLoading','$ionicPlatform',
+function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading,$ionicPlatform) {
 
   $scope.init = function() {
+
+    var customBackButton = function() {};
+
+    // registerBackButtonAction() returns a function which can be used to deregister it
+    var deregisterBackButtonAction = $ionicPlatform.registerBackButtonAction(
+        customBackButton, 101
+    );
+
+    $scope.$on('$destroy', function() {
+        deregisterBackButtonAction();
+    });
+
     if(window.localStorage.getItem("dadosUsuario") != null) {
           var user = JSON.parse(window.localStorage.getItem("dadosUsuario"));
     } else {
