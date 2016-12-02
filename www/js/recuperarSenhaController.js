083baa8c4aec8ec9057516c4328caa58781b5d9a
Roberto B Sampaio Junior <roberto2011.junior@gmail.com>
2016-12-02 16:20:52 -0200

    Correções BUG.

--- www/js/recuperarSenhaController.js
+++ www/js/recuperarSenhaController.js
@@ -12,7 +12,7 @@ function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading) {
   $scope.init = function() {};
 
   $scope.recuperaPin = function(user) {
-    window.plugins.toast.show('recuperaPin=> '+JSON.stringify({data2: user}), 'long', 'center', function(a){}, function(b){});
+    //window.plugins.toast.show('recuperaPin=> '+JSON.stringify({data2: user}), 'long', 'center', function(a){}, function(b){});
 
     $ionicLoading.show({
       template: 'Carregando...'
@@ -51,7 +51,7 @@ function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading) {
   };
 
   $scope.recuperaSenha = function(user) {
-    window.plugins.toast.show('recuperaSenha=> '+JSON.stringify({data2: user}), 'long', 'center', function(a){}, function(b){});
+   // window.plugins.toast.show('recuperaSenha=> '+JSON.stringify({data2: user}), 'long', 'center', function(a){}, function(b){});
 
     $ionicLoading.show({
       template: 'Carregando...'
@@ -67,7 +67,7 @@ function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading) {
               return;
             }
 
-    var response = $http.post('http://ec2-52-67-37-24.sa-east-1.compute.amazonaws.com:8080/scgas/rest/usuarioservice/recuperaSenhaPorPIN', {user:user,timeout: 5000});
+    var response = $http.post('http://ec2-52-67-37-24.sa-east-1.compute.amazonaws.com:8080/scgas/rest/usuarioservice/recuperaSenhaPorPIN',user);
 
     // Response retornado com sucesso
     response.success(function(data, status, headers, config) {
@@ -82,9 +82,7 @@ function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading) {
     // Response retornado com erros
     response.error(function(data, status, headers, config) {
          var errorMessage = 'Não foi possível recuperar a senha!';
-         if(status == 500)
-           errorMessage = 'Problemas com o servidor. Tente novamente mais tarde.';
-
+         
         $ionicLoading.hide();
 
         window.plugins.toast.show(errorMessage, 'long', 'center', function(a){}, function(b){});
