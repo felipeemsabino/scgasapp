7ccd29b7f3eecea1b08c3871b49087926bed720c
Roberto B Sampaio Junior <roberto2011.junior@gmail.com>
2017-02-13 23:20:52 -0200

    Pluguin para links do APP

--- www/js/telaInicialController.js
+++ www/js/telaInicialController.js
@@ -29,6 +29,25 @@ function($scope, $stateParams, $state, $http, $ionicSideMenuDelegate, $ionicLoad
     //$state.go("app.mapa");
     $state.go("app.mapa_postos");
   };
+    
+  $scope.openInstaladoresCordovaWebView = function()
+  {
+     // Open cordova webview if the url is in the whitelist otherwise opens in app browser
+        window.open('http://www.inmetro.gov.br/inovacao/oficinas/lista_oficinas.asp?end=1&descr_estado=sc','_blank');    
+  };
+    
+  $scope.openInspecaoCordovaWebView = function()
+  {
+     // Open cordova webview if the url is in the whitelist otherwise opens in app browser
+        window.open('http://www.inmetro.gov.br/organismos/resultado_consulta.asp?Seq_Tipo_Relacionamento=13&Ind_Status=A&Sig_Pais=BRA&Ind_Ordenacao=C&Sig_Uf=SC','_blank');    
+  };
+    
+  $scope.openLicenciamentoCordovaWebView = function()
+  {
+     // Open cordova webview if the url is in the whitelist otherwise opens in app browser
+        window.open('http://www.detran.sc.gov.br/index.php/institucional/endereco-ciretrans','_blank');    
+  };
+ 
 
   $scope.init = function() {
 
